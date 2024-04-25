6fb9beca086eec1c0337c4c379505624eefaafe1
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2024-04-26 02:20:04 +0800

    Support uppercase book

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -2,16 +2,16 @@ from citationparser import parse
 
 
 def test_simple():
-    citation = parse("matthew 1:2")
-    assert citation.book == "matthew"
+    citation = parse("Matthew 1:2")
+    assert citation.book == "Matthew"
     assert citation.start.chapter == 1
     assert citation.start.verse == 2
     assert citation.end == citation.start
 
 
 def test_span_verse():
-    citation = parse("matthew 1:2-3")
-    assert citation.book == "matthew"
+    citation = parse("Mark 1:2-3")
+    assert citation.book == "Mark"
     assert citation.start.chapter == 1
     assert citation.start.verse == 2
     assert citation.end.chapter == 1
@@ -19,8 +19,8 @@ def test_span_verse():
 
 
 def test_span_chapter():
-    citation = parse("matthew 1:2-3:4")
-    assert citation.book == "matthew"
+    citation = parse("Luke 1:2-3:4")
+    assert citation.book == "Luke"
     assert citation.start.chapter == 1
     assert citation.start.verse == 2
     assert citation.end.chapter == 3
