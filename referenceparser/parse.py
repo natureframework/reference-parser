d235050e336e11c371e869ba8cca28c9ac9199a2
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2024-05-14 10:18:55 +0800

    Fix return type

--- referenceparser/parse.py
+++ referenceparser/parse.py
@@ -1,7 +1,8 @@
+from .classes.reference import Reference
 from .grammar import grammar
 from .visitor import visitor
 
 
-def parse(text: str) -> dict:
+def parse(text: str) -> Reference:
     tree = grammar.parse(text)
     return visitor.visit(tree)
